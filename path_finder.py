20efc5f832fa21b099575debeebafac7c1aa9990
Dvwhite81 <Dvwhite1981@gmail.com>
2023-09-07 09:32:16 -0400

    Phase 3 - Complete

--- path_finder.py
+++ path_finder.py
@@ -37,6 +37,21 @@ class KnightPathFinder:
         self._considered_positions.update(positions)
         return positions
 
+    def build_move_tree(self):
+        moves = [self._root]
+
+        while moves:
+            current = moves[0]
+            position = current.value
+            new_moves = self.new_move_positions(position)
+
+            for move in new_moves:
+                node = Node(move)
+                current.add_child(node)
+                moves.append(node)
+
+            moves.pop(0)
+
 
 # Tests
 # finder = KnightPathFinder((0, 0))
@@ -47,3 +62,7 @@ class KnightPathFinder:
 
 # f3 = KnightPathFinder((1, 8))
 # print(f3.new_move_positions((1, 8)))
+
+finder = KnightPathFinder((0, 0))
+finder.build_move_tree()
+print(finder._root.children)
